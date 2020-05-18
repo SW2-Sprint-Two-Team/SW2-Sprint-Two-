dd1d806111ab23a1f230524b8fc3691715c22467
MohamedNabil12 <anamessironaldo@gmail.com>
2020-05-19 00:23:43 +0200

    create database for worker dashboard
    create database table and controller and model and view for worker

---
+++ PROJECT-SW-master/SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Worker]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity(1,1), 
+    [Fname] VARCHAR(50) NULL, 
+    [Lname] VARCHAR(50) NULL, 
+    [Age] INT NULL, 
+    [Address] VARCHAR(200) NULL, 
+    [SSN] NVARCHAR(50) NULL, 
+    [PhoneNumber] NVARCHAR(50) NULL
+)
